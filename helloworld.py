868f5a4c6e66225791b481b27d994877f49b7025
programmerLY <821517836@qq.com>
2020-03-28 22:32:57 +0800

    测试

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,23 @@
-print("124")
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+region = ['Azerbaijan','Bahamas', 'Bangladesh', 'Belize', 'Bhutan',
+          'Cambodia', 'Cameroon', 'Cape Verde', 'Chile', 'China']   #10个
+
+kind = ['Afforestation & reforestation', 'Biofuels', 'Biogas', 'Biomass', 'Cement']   #5个
+
+np.random.seed(20180316)
+arr_region = np.random.choice(region, size=(200,))
+list_region = list(arr_region)
+
+arr_kind = np.random.choice(kind, size=(200,))
+list_kind = list(arr_kind)
+
+values = np.random.randint(100, 200, 200)
+list_values = list(values)
+
+df = pd.DataFrame({'region':list_region,'kind': list_kind,'values':list_values})
+
+df.head(10)
